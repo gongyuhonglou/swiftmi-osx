39482e7e320092c3d0fab846dae53d4ef5623583
feiin <feiin@qq.com>
2016-05-09 17:04:28 +0800

    add status item button

--- swiftmi/swiftmi/AppDelegate.swift
+++ swiftmi/swiftmi/AppDelegate.swift
@@ -12,14 +12,24 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "code")
+            button.imagePosition = .ImageLeft
+            button.action = #selector(togglePopover)
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+    }
+
+    func togglePopover() {
+        print("toggle Popover ")
     }
 
 
