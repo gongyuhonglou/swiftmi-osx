09a9b1665c4a627a029fdc1c28e7217fa3e4e321
feiin <feiin@qq.com>
2016-05-09 13:54:08 +0800

    init project

---
+++ swiftmi/swiftmi/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  swiftmi
+//
+//  Created by yangyin on 16/5/9.
+//  Copyright © 2016年 swiftmi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
