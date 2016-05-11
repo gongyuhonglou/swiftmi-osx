10b5a99e1718c068b06953641175948b2a47eb62
feiin <feiin@qq.com>
2016-05-11 23:36:47 +0800

    add SMPopover

---
+++ swiftmi/swiftmi/SMPopover.swift
@@ -0,0 +1,18 @@
+//
+//  SMPopver.swift
+//  swiftmi
+//
+//  Created by yangyin on 16/5/11.
+//  Copyright © 2016年 swiftmi. All rights reserved.
+//
+
+import Cocoa
+
+class SMPopover {
+    private class var appDelegate: AppDelegate {
+        return NSApplication.sharedApplication().delegate as! AppDelegate
+    }
+    
+   
+    
+}
