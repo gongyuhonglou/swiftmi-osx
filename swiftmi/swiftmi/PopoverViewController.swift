9f07b782efb3877caa22b652a3271ddeb9798d82
feiin <feiin@qq.com>
2016-05-16 12:31:58 +0800

    add PopoverViewController

---
+++ swiftmi/swiftmi/PopoverViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PopoverViewController.swift
+//  swiftmi
+//
+//  Created by yangyin on 16/5/16.
+//  Copyright © 2016年 swiftmi. All rights reserved.
+//
+
+import Cocoa
+
+class PopoverViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
